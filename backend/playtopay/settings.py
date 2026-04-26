dff1f2ae7043b9da4c7a5dc1f2f47f48f883cd01
mrrr7 <mrrr7890@gmail.com>
2026-04-26 23:34:15 +0530

    fix: Parse DATABASE_URL natively to support Render deployments directly

--- backend/playtopay/settings.py
+++ backend/playtopay/settings.py
@@ -49,17 +49,34 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "playtopay.wsgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.postgresql"),
-        "NAME": os.environ.get("POSTGRES_DB", "playtopay"),
-        "USER": os.environ.get("POSTGRES_USER", "playtopay"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "playtopay"),
-        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
-        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
-        "CONN_MAX_AGE": 60,
+import urllib.parse
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url:
+    url = urllib.parse.urlparse(database_url)
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": url.path[1:],
+            "USER": url.username,
+            "PASSWORD": url.password,
+            "HOST": url.hostname,
+            "PORT": url.port or "5432",
+            "CONN_MAX_AGE": 60,
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.postgresql"),
+            "NAME": os.environ.get("POSTGRES_DB", "playtopay"),
+            "USER": os.environ.get("POSTGRES_USER", "playtopay"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "playtopay"),
+            "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+            "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+            "CONN_MAX_AGE": 60,
+        }
+    }
 
 if os.environ.get("USE_SQLITE") == "1":
     DATABASES["default"] = {
